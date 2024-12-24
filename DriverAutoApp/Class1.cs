c590afda3cfa430b12e8a9a8cc3b35fdca59f890
315 <315@315-41>
2024-12-24 10:33:27 +0300

    Добавьте файлы проекта.

---
+++ DriverAutoApp/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DriverAutoLibrary
+{
+    public class Auto
+    {
+        public string Title { get; set; }
+        public string Number { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine($"Автомобиль: {Title}, Госномер: {Number}");
+        }
+    }
+
+    public class Driver
+    {
+        public string Name { get; set; }
+        public string Class { get; set; } //Григорьев(Родькин) 34 ИС
+        public List<Auto> Autos { get; set; } = new List<Auto>();
+
+        public void Show()
+        {
+            Console.WriteLine($"Водитель: {Name}, Квалификация: {Class}");
+        }
+
+        public void ShowAll()
+        {
+            foreach (var auto in Autos)
+            {
+                auto.Show();
+            }
+        }
+    }
+}
---
+++ DriverAutoApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using DriverAutoLibrary;
+using System;
+
+namespace DriverAutoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver driver = new Driver
+            {
+                Name = "Григорьев Илья Игоревич",
+                Class = "A"
+            };
+
+            driver.Autos.Add(new Auto { Title = "Toyota", Number = "A123BC" });
+            driver.Autos.Add(new Auto { Title = "Honda", Number = "B456CD" });
+            driver.Autos.Add(new Auto { Title = "Ford", Number = "C789EF" }); //Григорьев(Родькин) 34ИС
+
+            driver.Show();
+            driver.ShowAll();
+
+
+            Console.ReadKey();
+        }
+    }
+}
